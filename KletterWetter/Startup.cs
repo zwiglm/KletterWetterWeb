151ae746bd09cc581d5687bc1e57ef0a2907e1df
m.papa <m.papa@d01d30fd-290b-8545-b148-9c0714406479>
2017-07-17 23:58:40 +0000

    Initial

---
+++ KletterWetter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KletterWetter.Startup))]
+namespace KletterWetter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
